903c7ef8154b5e1ed3229c528f5b73072bb18041
Peter John Alvarado <peterjohn@gorated.ph>
2020-12-11 22:50:53 +0800

    [feat] Day 2 Part 2 Solution

--- 2020/2/day_two.go
+++ 2020/2/day_two.go
@@ -14,6 +14,67 @@ func checkError(err error) {
 	}
 }
 
+func problemOne(collection []string) int {
+	validPword := 0
+	for _, v := range collection {
+		splitted := strings.Split(v, " ")
+		var values = make([]string, 3, 3)
+		for idx, s := range splitted {
+			values[idx] = s
+		}
+		markers := strings.Split(values[0], "-")
+		char := strings.Split(values[1], ":")
+
+		match := 0
+		for _, c := range values[2] {
+			if char[0] == string(c) {
+				match++
+			}
+		}
+
+		min, _ := strconv.Atoi(markers[0])
+		max, _ := strconv.Atoi(markers[1])
+		if match >= min && match <= max {
+			validPword++
+		}
+	}
+	return validPword
+}
+
+func problemTwo(collection []string) int {
+	validPword := 0
+	for _, v := range collection {
+		splitted := strings.Split(v, " ")
+		var values = make([]string, 3, 3)
+		for idx, s := range splitted {
+			values[idx] = s
+		}
+		markers := strings.Split(values[0], "-")
+		char := strings.Split(values[1], ":")
+
+		var stringCollection []string
+		for _, c := range values[2] {
+			stringCollection = append(stringCollection, string(c))
+		}
+
+		min, _ := strconv.Atoi(markers[0])
+		max, _ := strconv.Atoi(markers[1])
+
+		nMin := min - 1
+		nMax := max - 1
+
+		fmt.Printf("Collection %v Markers %v \n", stringCollection, markers)
+		matchingChar := char[0]
+		fmt.Printf("Matcher %v-%v-%v \n", matchingChar, stringCollection[nMin], stringCollection[nMax])
+		fmt.Printf("Has Matching %v\n", stringCollection[nMin] == matchingChar || stringCollection[nMax] == matchingChar)
+		fmt.Printf("Has No Matching %v\n", !(stringCollection[nMin] == matchingChar && stringCollection[nMax] == matchingChar))
+		if (stringCollection[nMin] == matchingChar || stringCollection[nMax] == matchingChar) && !(stringCollection[nMin] == matchingChar && stringCollection[nMax] == matchingChar) {
+			validPword++
+		}
+	}
+	return validPword
+}
+
 func main() {
 	data, err := os.Open("./input.txt")
 
@@ -30,37 +91,9 @@ func main() {
 		collection = append(collection, content)
 	}
 
-	validPword := 0
-	var matches []string
+	ans := problemOne(collection)
+	fmt.Printf("Problem 1 Answer: %v \n", ans)
 
-	for _, v := range collection {
-		splitted := strings.Split(v, " ")
-		var values = make([]string, 3, 3)
-		for idx, s := range splitted {
-			values[idx] = s
-		}
-		markers := strings.Split(values[0], "-")
-		char := strings.Split(values[1], ":")
-		fmt.Printf("Markers %v-%v \n", markers[0], markers[1])
-		fmt.Printf("Char %s\n", char[0])
-
-		match := 0
-		for _, c := range values[2] {
-			fmt.Printf("Char %s-%s\n", char[0], string(c))
-			if char[0] == string(c) {
-				fmt.Println("Match \n")
-				match++
-			}
-		}
-
-		min, _ := strconv.Atoi(markers[0])
-		max, _ := strconv.Atoi(markers[1])
-		if match >= min && match <= max {
-			matches = append(matches, v)
-			validPword++
-		}
-	}
-
-	fmt.Printf("Matches: %v \n", matches)
-	fmt.Printf("Answer: %v \n", validPword)
+	ans = problemTwo(collection)
+	fmt.Printf("Problem 2 Answer: %v \n", ans)
 }
