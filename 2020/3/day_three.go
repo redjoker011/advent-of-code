ceb617648c9c5265fed914f822a845f67f9e0c3a
Peter John Alvarado <peterjohn@gorated.ph>
2020-12-13 03:51:46 +0800

    [feat] Day three solution

---
+++ 2020/3/day_three.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+var tree string = "#"
+
+func problemOne(terrain []string, down int, right int) int {
+	xOffset := 0
+	yOffset := 0
+	match := 0
+
+	for yOffset < len(terrain) {
+		line := terrain[yOffset]
+
+		if line == "" {
+			break
+		}
+
+		if string(line[xOffset]) == tree {
+			match++
+		}
+
+		yOffset += down
+		xOffset = (xOffset + right) % len(line)
+	}
+	return match
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	data, err := ioutil.ReadFile("./input.txt")
+	checkError(err)
+
+	routeMap := strings.Split(string(data), "\n")
+	fmt.Printf("Answer to Problem 1 is %v\n", problemOne(routeMap, 1, 3))
+
+	fmt.Println("Answer to part 2:", (problemOne(routeMap, 1, 1) *
+		problemOne(routeMap, 1, 3) *
+		problemOne(routeMap, 1, 5) *
+		problemOne(routeMap, 1, 7) *
+		problemOne(routeMap, 2, 1)))
+}
