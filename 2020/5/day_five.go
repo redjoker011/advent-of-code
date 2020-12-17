8c49bd62a6e4b91f6427aff5ae2b6e98c25359af
Peter John Alvarado <peterjohn@gorated.ph>
2020-12-17 22:01:55 +0800

    [feat] Update Day 5 solution using binary conversion

--- 2020/5/day_five.go
+++ 2020/5/day_five.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 )
 
@@ -14,80 +15,41 @@ var frontMarker = "F"
 var leftMarker = "L"
 var rightMarker = "R"
 
+var lowerPointer = "0"
+var higherPointer = "1"
+
 func checkError(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
-func highestSeatID(passes []string) int {
-	best := 0
+func decConverter(char string) string {
+	if char == frontMarker || char == leftMarker {
+		return lowerPointer
+	}
+	return higherPointer
+}
+
+// Convert Code into Binary and then Convert Binary to Decimal using
+// strconv package
+func highestSeatID(passes []string) int64 {
+	var best int64 = 0
 	for i := 0; i < len(passes)-1; i++ {
 		code := passes[i]
 		rowCode := code[:7]
 		colCode := code[7:]
 
-		row := 0
-		col := 0
+		rowCode = strings.ReplaceAll(string(rowCode), frontMarker, lowerPointer)
+		rowCode = strings.ReplaceAll(string(rowCode), backMarker, higherPointer)
 
-		lPointer := 0
-		rPointer := 0
+		colCode = strings.ReplaceAll(string(colCode), leftMarker, lowerPointer)
+		colCode = strings.ReplaceAll(string(colCode), rightMarker, higherPointer)
 
-		for r := 0; r <= len(rowCode)-1; r++ {
-			char := string(rowCode[r])
-			if r == 0 {
-				if char == frontMarker {
-					lPointer = 0
-					rPointer = 63
-					fmt.Printf("Initial F Keeping row %v through %v\n", lPointer, rPointer)
-				} else {
-					lPointer = 64
-					rPointer = rowsCount
-					fmt.Printf("Initial B Keeping row %v through %v\n", lPointer, rPointer)
-				}
-			} else {
-				if char == frontMarker {
-					rPointer = lPointer + ((rPointer - lPointer) / 2)
-					// rPointer = rPointer - ((rPointer-lPointer)+1)/2
-					fmt.Printf("F Keeping row %v through %v\n", lPointer, rPointer)
-					// fmt.Printf("R Pointer Set To %v\n", rPointer)
-				} else {
-					//lPointer = (rPointer / 2) + 1
-					lPointer = lPointer + (((rPointer + 1) - lPointer) / 2)
-					fmt.Printf("B Keeping row %v through %v\n", lPointer, rPointer)
-					// fmt.Printf("L Pointer Set To %v\n", lPointer)
-				}
-			}
-		}
-		// Final Row
-		fmt.Printf("Pointers L %v, R %v\n", lPointer, rPointer)
-		row = Max(lPointer, rPointer)
+		row, _ := strconv.ParseInt(rowCode, 2, 64)
+		col, _ := strconv.ParseInt(colCode, 2, 64)
 
-		llPointer := 0
-		rrPointer := 0
-
-		for c := 0; c <= len(colCode)-1; c++ {
-			char := string(colCode[c])
-
-			if c == 0 {
-				if char == leftMarker {
-					rrPointer = colsCount
-				} else {
-					llPointer = 4
-					rrPointer = colsCount
-				}
-			} else {
-				if char == leftMarker {
-					rrPointer = llPointer + ((rrPointer - llPointer) / 2)
-					fmt.Printf("Keeping col %v through %v\n", llPointer, rrPointer)
-				} else {
-					llPointer = llPointer + (((rrPointer + 1) - llPointer) / 2)
-					fmt.Printf("Keeping col %v through %v\n", llPointer, rrPointer)
-				}
-			}
-		}
-		col = Max(llPointer, rrPointer)
-
+		fmt.Printf("Row set to %v and Col set to %v\n", row, col)
 		prod := (row * 8) + col
 		best = Max(best, prod)
 	}
@@ -95,7 +57,7 @@ func highestSeatID(passes []string) int {
 	return best
 }
 
-func Max(x, y int) int {
+func Max(x, y int64) int64 {
 	if x < y {
 		return y
 	}
@@ -103,7 +65,7 @@ func Max(x, y int) int {
 }
 
 func main() {
-	data, err := ioutil.ReadFile("./input_t.txt")
+	data, err := ioutil.ReadFile("./input.txt")
 	checkError(err)
 
 	passes := strings.Split(string(data), "\n")
