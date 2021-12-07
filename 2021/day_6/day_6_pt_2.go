768a0cabafb31d64b3d38604c81015658ecae7b8
Peter John Alvarado <redjoker011@gmail.com>
2021-12-07 19:40:24 +0800

    feat: Add comment on day 6

--- 2021/day_6/day_6_pt_2.go
+++ 2021/day_6/day_6_pt_2.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	data, err := ioutil.ReadFile("./2021/day_6/input.txt")
+	data, err := ioutil.ReadFile("./2021/day_6/input_t.txt")
 
 	if err != nil {
 		panic(err)
@@ -17,6 +17,8 @@ func main() {
 
 	input := strings.Split(string(data), "\n")
 
+	// Breeding take 9 days so create a variable that will track the count of each
+	// fish breeding timer
 	var fish = make([]int, 9)
 	for _, i := range strings.Split(input[0], ",") {
 		n, err := strconv.Atoi(i)
@@ -24,15 +26,20 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		// Track fish based on timer 0 - 8 days
 		fish[n]++
 	}
+	fmt.Println(fish)
 
 	// Loop for 256 days
 	for j := 0; j < 256; j++ {
-		fmt.Printf("Day %v \n", j)
+		fmt.Printf("Day %v; Fish %v \n", j+1, fish)
+		// Update Tracker each day
 		fish = step(fish)
 	}
 
+	// Get the sum
 	sum := 0
 	for _, v := range fish {
 		sum += v
@@ -41,14 +48,24 @@ func main() {
 	fmt.Println("Sum %v", sum)
 }
 
+// Update Tracker
 func step(fish []int) []int {
+	// Create a variable to track breeding timer
 	var next = make([]int, 9)
 
+	// Reassign timer from previous day to current day
+	// 0 = 1; 1 = 2 and so on..
 	for i := 1; i < 9; i++ {
 		next[i-1] = fish[i]
 	}
 
+	fmt.Printf("N %v \n", next)
+
+	// Assume fish[0] will breed new fish reset timer
+	// for new breed and parent
 	next[6] += fish[0]
 	next[8] += fish[0]
+
+	fmt.Printf("M %v \n", next)
 	return next
 }
